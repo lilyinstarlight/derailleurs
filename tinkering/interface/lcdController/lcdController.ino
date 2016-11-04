787c40a61f772cb05fd4e9b6559915fce6d78294
ryan-becwar <rbecwar1@gmail.com>
2016-11-04 11:06:17 -0400

    Added unit tests and current gear display

--- tinkering/interface/lcdController/lcdController.ino
+++ tinkering/interface/lcdController/lcdController.ino
@@ -9,6 +9,7 @@ Pin mapping follows as defined below, with vcc and gnd going directly between th
 dev board and the screen, and a 330 ohm resistor between the backlight pin(P1_2), and the screen.
 Note that the pin labled SCL* on the dev board is used instead of SCLK for the screen SCLK input.
 
+Run tests by uncommenting the define TEST and calling the runTests() function
 Ryan Becwar
 */
 
@@ -27,9 +28,14 @@ Ryan Becwar
 #define LCD_X     84
 #define LCD_Y     48
 
+//Uncomment if you want to run in test mode
+#define TEST
+
+
 //Cadence will never go above 200, so 8 bit ints work well
 uint8_t targetCadence = 0;
 uint8_t currentCadence = 0;
+uint8_t gearPos = 0;
 
 volatile int flag = HIGH;
 
@@ -222,8 +228,16 @@ void writeCadence(void) {
   LcdString("            ");
   LcdString(curBuf);
   LcdString("            ");
-  LcdString("            ");
+  LcdString("Gear:      5");
 
+  #ifdef TEST
+    Serial.println(tarBuf);
+    Serial.println("____________");
+    Serial.println("            ");
+    Serial.println(curBuf);
+    Serial.println("            ");
+    Serial.println("Gear:      5");
+   #endif
 }
 
 /*Functions to handle buttons to increase or decrease target cadence.*/
@@ -239,9 +253,101 @@ void bDown(){
   targetCadence--;
 }
 
+uint8_t setCurrent(uint8_t cadence){
+  currentCadence = cadence;
+  return currentCadence;
+}
+
+uint8_t setGear(uint8_t gear){
+  if(gear < 1)
+    gear = 0;
+  if(gear > 8)
+    gear = 8;
+   gearPos = gear;
+}
+
+//Writes output to Serial, always returns true
+int testWrite(void){
+  writeCadence();
+  return 0;
+}
+
+//Tests the setCurrent function
+int testCurrentAdjust(){
+  setCurrent(124);
+  if(currentCadence == 124)
+    return 0;
+  else
+    return 1;
+}
+
+int testSetGear(){
+  setGear(5);
+  if(gearPos == 5)
+    return 0;
+  else
+    return 1;
+}
+
+int testSetGearTooHigh(){
+  setGear(200);
+  if(gearPos == 8)
+    return 0;
+  else
+    return 1;
+}
+
+//Tests return 0 if pass, 1 if fail
+//This should definitley be restructured to use function pointers and iteration, but is used now in interest of time.
+void runTests(void){
+  Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  Serial.println("TEST: testWrite");
+  Serial.println("OUTPUT:");
+  if(!testWrite()){
+    Serial.println("PASSED");
+  } else {
+    Serial.println("FAILED");
+  }
+  Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  Serial.println();
+  
+  Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  Serial.println("TEST: testCurrentAdjust");
+  Serial.println("OUTPUT:");
+  if(!testCurrentAdjust()){
+    Serial.println("PASSED");
+  } else {
+    Serial.println("FAILED");
+  }
+  Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  Serial.println();
+  
+  Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  Serial.println("TEST: testSetGear");
+  Serial.println("OUTPUT:");
+  if(!testSetGear()){
+    Serial.println("PASSED");
+  } else {
+    Serial.println("FAILED");
+  }
+  Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  Serial.println();
+  
+  Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  Serial.println("TEST: testSetGearTooHigh");
+  Serial.println("OUTPUT:");
+  if(!testSetGearTooHigh()){
+    Serial.println("PASSED");
+  } else {
+    Serial.println("FAILED");
+  }
+  Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  Serial.println();
+}
 
 void setup(void)
 {
+  Serial.begin(9600);
   //Setup Buttons
   //Enable internal pullup. 
   //These will be changed to the button pins when connected to the board, curretnly represents redboard buttons
@@ -256,6 +362,8 @@ void setup(void)
   targetCadence = 73;
   currentCadence = 25;
   writeCadence();
+  
+  runTests();
 }
 
 void loop(void)
